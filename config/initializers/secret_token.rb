1c7190bd57260ba9ce817ae92fd72891c6fb1140
Alfonso Maestro <fonelmaestro@gmail.com>
2013-08-07 13:35:55 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VineVideos::Application.config.secret_token = '490196392d1e7fadaf1f5e65f9f87fdfade47b37c768d20cc48f401cec6e07032c5f45c8c5ee3dd198e85aa791aa2566ee6707e76f5de88961aebb983859ca15'
